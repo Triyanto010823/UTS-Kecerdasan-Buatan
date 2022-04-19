dab01ae7f48e7f765e7722e9309934406e22191d
Triyanto010823 <104020336+Triyanto010823@users.noreply.github.com>
2022-04-19 23:00:37 +0700

    Add files via upload

---
+++ Triyanto.py
@@ -0,0 +1,8 @@
+num = float(input("Enter a number: "))
+if num >= 0:
+    if num == 0:
+        print("Kosong/Nol")
+    else:
+        print("Bilangan Positif")
+else:
+    print("Bilangan Negatif")
